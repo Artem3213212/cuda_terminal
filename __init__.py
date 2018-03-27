9f28c96a4e9938ada8dbba8f0bc6f1ad8c6857a1
Alexey <Alexey-T@users.noreply.github.com>
2018-03-27 17:36:56 +0300

    Update __init__.py

--- __init__.py
+++ __init__.py
@@ -1,16 +1,15 @@
-from cudatext import *
 import sys
+import os
+from cudatext import *
 
 class Command:
-    def Open(self):
-        try:
-            MainForm = dlg_proc(0, DLG_CREATE)
-            app_proc(PROC_BOTTOMPANEL_ADD_DIALOG, tuple(["test", MainForm, "terminal.png"]))
-            #dlg_proc(MainForm, DLG_CTL_ADD, param="")#ALIGN_CLIENT
-            #dlg_proc()
-        except Exception as e:
-            msg_box(str(e), MB_OK)
+    def open(self):
+        self.h_dlg =  dlg_proc(0, DLG_CREATE)
+        app_proc(PROC_BOTTOMPANEL_ADD_DIALOG, ("Terminal", self.h_dlg, "terminal.png"))
+        #dlg_proc(MainForm, DLG_CTL_ADD, param="")#ALIGN_CLIENT
+
     def on_console(self, ed_self, text):
         pass
+    
     def on_console_nav(self, ed_self, text):
         pass
