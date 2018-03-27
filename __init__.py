ada3a5121a52103bccad18a3d2a538b1aa2c5f61
Alexey <Alexey-T@users.noreply.github.com>
2018-03-27 17:38:57 +0300

    Update __init__.py

--- __init__.py
+++ __init__.py
@@ -2,10 +2,12 @@ import sys
 import os
 from cudatext import *
 
+fn_icon = os.path.join(os.path.dirname(__file__), 'terminal.png')
+
 class Command:
     def open(self):
         self.h_dlg =  dlg_proc(0, DLG_CREATE)
-        app_proc(PROC_BOTTOMPANEL_ADD_DIALOG, ("Terminal", self.h_dlg, "terminal.png"))
+        app_proc(PROC_BOTTOMPANEL_ADD_DIALOG, ("Terminal", self.h_dlg, fn_icon))
         #dlg_proc(MainForm, DLG_CTL_ADD, param="")#ALIGN_CLIENT
 
     def on_console(self, ed_self, text):
